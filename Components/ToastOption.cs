9c8754a1391df16de6cf3f4fb858253cec82621a
Backiaraj <backiaraj.kanagaraj@syncfusion.com>
2026-01-09 16:48:02 +0530

    Updated Show or Hide Toast sample

--- Components/ToastOption.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace ShowOrHideToast.Components
-{
-    public class ToastOption
-    {
-        public string Title { get; set; }
-        public string Content { get; set; }
-    }
-}
--- Program.cs
+++ Program.cs
@@ -1,15 +1,11 @@
-using Microsoft.AspNetCore.Components;
-using Microsoft.AspNetCore.Components.Web;
 using ShowOrHideToast.Components;
-using ShowOrHideToast.Data;
 using Syncfusion.Blazor;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddRazorPages();
-builder.Services.AddServerSideBlazor();
-builder.Services.AddSingleton<WeatherForecastService>();
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
 builder.Services.AddSingleton<ToastService>();
 builder.Services.AddSyncfusionBlazor();
 
@@ -18,18 +14,18 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Error");
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
 app.UseHttpsRedirection();
 
-app.UseStaticFiles();
 
-app.UseRouting();
+app.UseAntiforgery();
 
-app.MapBlazorHub();
-app.MapFallbackToPage("/_Host");
+app.MapStaticAssets();
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
 
 app.Run();
