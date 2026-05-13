ad6f787c7f0dbd219704fc01d95283fef9b68a23
JafarAli <jafarali.s@syncfusion.com>
2026-05-13 21:12:28 +0530

    882076: Need to include ToastService and ToastOption classes for the demo repository.

---
+++ Controller/ToastService.cs
@@ -0,0 +1,17 @@
+﻿namespace ShowOrHideToast.Controller
+{
+    public class ToastOption
+    {
+        public string Title { get; set; }
+        public string Content { get; set; }
+    }
+    public class ToastService
+    {
+        public event Action<ToastOption> ShowToastTrigger;
+        public void ShowToast(ToastOption options)
+        {
+            //Invoke ToastComponent to update and show the toast with messages  
+            this.ShowToastTrigger.Invoke(options);
+        }
+    }
+}
